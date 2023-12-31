bbd3e69e754ac9557e25ceec7ad8fc639bd9631a
object-ions <moshikoatia@gmail.com>
2023-12-31 11:20:00 -0800

    Add deme text to practice style with scss

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,29 @@ import "./App.scss";
 
 function App() {
   return (
-    <>
-      <h1>Sass Sandbox</h1>
-    </>
+    <div>
+      <header>
+        <h1>Sass Sandbox</h1>
+      </header>
+      <section className="section section-a">
+        <h3>Section A</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          fermentum eget ante sed hendrerit. Mauris semper eget ante at
+          lobortis.
+        </p>
+        <a href="#">Read More</a>
+      </section>
+      <section className="section section-b">
+        <h3>Section B</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          fermentum eget ante sed hendrerit. Mauris semper eget ante at
+          lobortis.
+        </p>
+        <a href="#">Read More</a>
+      </section>
+    </div>
   );
 }
 
